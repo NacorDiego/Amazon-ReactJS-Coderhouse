e8b03033b8201f1628647a5437a592f7bfbf0cb3
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2022-07-14 14:15:05 -0300

    Commit 29: Se integra logica del carrito en el catalogo.

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -4,14 +4,17 @@ import ItemAddCart from "./ItemAddCart";
 import { useState } from "react";
 import ItemDetail from "./ItemDetail";
 
-function Item({ id, marca, modelo, stock, precio, img }) {
+function Item({ producto }) {
+
+    const { id, marca, modelo, stock, precio, img } = producto;
+
     const [detail, setDetail] = useState(false)
 
     return (
         <div className="bg-light-100 ease-linear duration-300 hover:bg-blue-700 hover:text-light-100 hover:shadow-lg hover:shadow-blue-900/40 hover:ease-linear hover:duration-300 hover:scale-110 w-72 h-auto rounded-xl pb-10">
             <ItemImg id={id} img={img} />
             <ItemText id={id} detail={detail} setDetail={setDetail} marca={marca} modelo={modelo} stock={stock} precio={precio} />
-            <ItemAddCart id={id} stock={stock} />
+            <ItemAddCart producto={producto} stock={stock} />
             {detail && <ItemDetail />}
         </div>
     );
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -4,7 +4,7 @@ function ItemList({ productos }) {
     return (
         <div className="w-full flex justify-start flex-wrap items-start gap-16">
             {productos.map(
-                i => <Item key={i.id} id={i.id} marca={i.marca} modelo={i.modelo} stock={i.stock} precio={i.precio} img={i.img} />
+                i => <Item producto={i} key={i.id} />
             )}
         </div>
     );
