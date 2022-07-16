15f76190871aa325a6c0c5d4d0485d09f2ac4846
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2022-07-15 22:35:20 -0300

    Commit 30: Se comienza a armar la estructura del /cart.

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,11 +1,14 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faCartShopping} from '@fortawesome/free-solid-svg-icons';
+import { Link } from "react-router-dom";
 
 function CartWidget() {
     return (
-        <button className="flex items-center">
-            <FontAwesomeIcon className="text-2xl text-white hover:text-light-300 ease-linear duration-200" icon={faCartShopping} />
-        </button>
+        <Link to="/cart">
+            <button className="flex items-center">
+                <FontAwesomeIcon className="text-2xl text-white hover:text-light-300 ease-linear duration-200" icon={faCartShopping} />
+            </button>
+        </Link>
     );
 }
 
--- src/pages/CartContainer.jsx
+++ src/pages/CartContainer.jsx
@@ -1,7 +1,13 @@
 function CartContainer() {
     return (
-        <>
-        </>
+        <div className="container mx-auto h-screen w-full">
+            <div className="h-4/6 w-full grid grid-rows-4 grid-flow-col gap-8 mt-20">
+                <div className="bg-sky col-span-4"></div>
+                <div className="bg-sky col-span-3 row-span-3"></div>
+                <div className="bg-sky col-span-1 row-span-1"></div>
+                <div className="bg-sky col-span-1 row-span-2"></div>
+            </div>
+        </div>
     );
 }
 
