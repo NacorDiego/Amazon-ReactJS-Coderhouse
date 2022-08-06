b9840be6d7ee5a049ff8f255ca04b080950c6e8e
Diego Ezequiel Nacor <diego.informaticaunlp@gmail.com>
2022-08-06 13:46:05 -0300

    Commit 53: Se modifica lógica del buyerForm. Se implementa el hook personalizado useForm, se implementan validaciones.

--- src/components/BuyerForm.jsx
+++ src/components/BuyerForm.jsx
@@ -10,9 +10,38 @@ import { faUser, faPhone, faAt } from "@fortawesome/free-solid-svg-icons";
 // import CartContext from "../store/cart-context";
 import { useForm } from "../hooks/useForm";
 
-const initialForm = {}
+const initialForm = {
+    name: "",
+    phone:"",
+    email: ""
+}
 
-const validationsForm = (form) => {}
+const validationsForm = (form) => {
+    let errors = {}
+    let regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/
+    let regexPhone = /^((\(?\d{3}\)?[-. ]?\d{4})|(\(?\d{4}\)?[-. ]?\d{3})|(\(?\d{5}\)?[-. ]?\d{2}))[-. ]?\d{4}$/
+    let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/
+
+    if (!form.name.trim()){ //.trim() evita los espacios en blanco al inicio y al final
+        errors.name = "El campo 'Nombre y apellido' es requerido"
+    } else if (!regexName.test(form.name.trim())){
+        errors.name = "El campo 'Nombre y apellido' solo acepta letras y espacios en blanco"
+    }
+
+    if (!form.phone.trim()){ //.trim() evita los espacios en blanco al inicio y al final
+        errors.phone = "El campo 'TeléfonoPhone es requerido"
+    } else if (!regexPhone.test(form.phone.trim())){
+        errors.phone = "El campo 'Teléfono' debe respetar el formato indicado"
+    }
+
+    if (!form.email.trim()){ //.trim() evita los espacios en blanco al inicio y al final
+        errors.email = "El campo 'Correo electrónico' es requerido"
+    } else if (!regexEmail.test(form.email.trim())){
+        errors.email = "El campo 'Correo electrónico' no es válido"
+    }
+
+    return errors
+}
 
 function BuyerForm() {
 
@@ -84,20 +113,23 @@ function BuyerForm() {
                             <label htmlFor="buyerName" className="p-3">
                                 <FontAwesomeIcon className="text-2xl text-yellow" icon={faUser} />
                             </label>
-                            <input className="w-full h-full rounded-xl p-5 text-xl font-light outline-none" name="buyerName" type="text" placeholder="Ingrese su nombre y apellido" onBlur={handleBlur} onChange={handleChange} value={form.name} required />
+                            <input className="w-full h-full rounded-xl p-5 text-xl font-light outline-none" name="name" type="text" placeholder="Ingrese su nombre y apellido" onBlur={handleBlur} onChange={handleChange} value={form.name} required />
                         </div>
+                        {errors.name && <p className="text-light-100">{errors.name}</p>}
                         <div className="w-5/6 h-12 bg-white rounded-xl flex items-center">
                             <label htmlFor="buyerPhone" className="p-3">
                                 <FontAwesomeIcon className="text-2xl text-yellow" icon={faPhone} />
                             </label>
-                            <input className="w-full h-full rounded-xl p-5 text-xl font-light outline-none" name="buyerPhone" type="number" placeholder="Ingrese su teléfono" onBlur={handleBlur} onChange={handleChange} value={form.phone} required />
+                            <input className="w-full h-full rounded-xl p-5 text-xl font-light outline-none" name="phone" type="text" placeholder="Ingrese su teléfono" onBlur={handleBlur} onChange={handleChange} value={form.phone} required />
                         </div>
+                        {errors.phone && <p className="text-light-100">{errors.phone}</p>}
                         <div className="w-5/6 h-12 bg-white rounded-xl flex items-center">
                             <label htmlFor="buyerEmail" className="p-3">
                                 <FontAwesomeIcon className="text-2xl text-yellow" icon={faAt} />
                             </label>
-                            <input className="w-full h-full rounded-xl p-5 text-xl font-light outline-none" name="buyerEmail" type="email" placeholder="Ingrese su correo electrónico" onBlur={handleBlur} onChange={handleChange} value={form.email} required />
+                            <input className="w-full h-full rounded-xl p-5 text-xl font-light outline-none" name="email" type="email" placeholder="Ingrese su correo electrónico" onBlur={handleBlur} onChange={handleChange} value={form.email} required />
                         </div>
+                        {errors.email && <p className="text-light-100">{errors.email}</p>}
                         <div className="w-5/6 h-12 flex justify-center items-center">
                             <input type="submit" value="Finalizar compra" className="w-3/6 h-12 bg-sky-100 rounded-xl text-white text-xl font-medium hover:bg-sky ease-linear duration-150" />
                         </div>
